c001e12d61c99032f9c685c8b5af3f7d189dbe35
deep-verma <75847946+deep-verma@users.noreply.github.com>
2022-09-22 18:40:40 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,11 +3,7 @@ import BottomTabNavigator from "./components/BottomTabNavigator";
 
 export default class App extends Component {
   render() {
-
-    //return <BottomTabNavigator>;
-    //<BottomTabNavigator />;
-    //return <BottomTabNavigator />;
-    //return <BottomTabNavigator> <BottomTabNavigator />;
+    return <BottomTabNavigator />;
     
   }
 }
